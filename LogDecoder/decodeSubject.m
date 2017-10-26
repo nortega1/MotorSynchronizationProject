da5ab81c4b0b0f4bdb9ea7ce9a1971f95547a45e
nortega1 <Nicole@Nicoles-MacBook-Pro.local>
2017-10-26 14:39:33 -0400

    removed files

---
+++ LogDecoder/decodeSubject.m
@@ -0,0 +1,7 @@
+% Decodes data files and fixes data files with corruptions
+
+% Decode all files
+num_trials = '3';
+
+system('"/Users/Nicole/Documents/JHU/LIMBS/TappingX/DecodeData/log" 7.12.2017 1 18')
+
